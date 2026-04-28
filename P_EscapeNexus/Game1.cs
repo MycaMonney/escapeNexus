ba760fcba1079b28edbc277367c657cdefeaf4b5
MycaMonney <myca.mnn@eduge.ch>
2026-04-28 16:03:58 +0200

    fonctionnement des interactions et changement de salle + console

--- P_EscapeNexus/Game1.cs
+++ P_EscapeNexus/Game1.cs
@@ -26,6 +26,7 @@ namespace P_EscapeNexus
         int currentMurIndex = 0;
         Texture2D[] murTextures;
         Texture2D[] introTextures;
+        List<string> messageConsole = new List<string>();
         private Song spaceSongMusic;
         private bool isMusicStarted;
         private bool isMuted = false;
@@ -47,9 +48,24 @@ namespace P_EscapeNexus
         private Texture2D textureMur2;
         private Texture2D textureMur3;
         private Texture2D textureMur4;
+        private Texture2D textureMur5;
+        private Texture2D textureMur6;
         private Texture2D textureFlecheDroite;
         private Texture2D textureFlecheGauche;
         private Texture2D textureBadge;
+        private Texture2D texturePorteOuverte;
+        private Texture2D textureOutilsAvecObj;
+        private Texture2D textureTournevis;
+        private Texture2D textureOutilsSansObj;
+        private Texture2D textureMur6SansOutils;
+        private Texture2D textureMurPanneauOuvert;
+        private SpriteFont fontConsole;
+
+        bool panneauOuvert = false;
+        bool tournevisPris = false;
+        bool afficheOutilsAvecObj = false;
+        bool porteOuverte = false;
+        bool dans2emePiece = false;
 
         // IA Probleme collision voir doc
         bool inputLocked = false;
@@ -65,13 +81,15 @@ namespace P_EscapeNexus
 
         protected override void Initialize()
         {
-            currentState = GameStateManager.Menu;
+            currentState = GameStateManager.Playing;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            fontConsole = Content.Load<SpriteFont>("DefaultFont");
 
             introTextures = new Texture2D[4];
             introTextures[0] = Content.Load<Texture2D>("H1");
@@ -90,24 +108,42 @@ namespace P_EscapeNexus
             textureCommande = Content.Load<Texture2D>("commande");
             textureInventaire = Content.Load<Texture2D>("inventaire");
             textureMur1 = Content.Load<Texture2D>("mur1");
+            texturePorteOuverte = Content.Load<Texture2D>("porteOuverte");
             textureMur2 = Content.Load<Texture2D>("mur2");
             textureMur3 = Content.Load<Texture2D>("mur3");
             textureMur4 = Content.Load<Texture2D>("mur4");
+            textureMur5 = Content.Load<Texture2D>("mur5");
+            textureMur6 = Content.Load<Texture2D>("mur6");
             textureFlecheDroite = Content.Load<Texture2D>("flecheDroite");
             textureFlecheGauche= Content.Load<Texture2D>("flecheGauche");
             textureBadge = Content.Load<Texture2D>("badge");
+            textureOutilsAvecObj = Content.Load<Texture2D>("outilsAvecObj");
+            textureTournevis = Content.Load<Texture2D>("tournevis");
+            textureOutilsSansObj = Content.Load<Texture2D>("outilsSansObj");
+            textureMur6SansOutils = Content.Load<Texture2D>("mur6SansOutils");
+            textureMurPanneauOuvert = Content.Load<Texture2D>("murPanneauOuvert");
 
             murTextures = new Texture2D[]
             {
                 textureMur1,
                 textureMur2,
                 textureMur3,
-                textureMur4
+                textureMur4,
+                textureMur5,
+                textureMur6
             };
 
             _debugTexture = new Texture2D(GraphicsDevice, 1, 1);
             _debugTexture.SetData(new[] { Color.White });
             spaceSongMusic = Content.Load<Song>("SpaceSong");
+        }
+        private void AjouterMessage(string message)
+        {
+            messageConsole.Add(message);
+            if(messageConsole.Count > 5)
+            {
+                messageConsole.RemoveAt(0);
+            }
         }
         private void DrawContour(Rectangle rectangle, Color color, int thickness)
         {
@@ -176,6 +212,29 @@ namespace P_EscapeNexus
                 Rectangle flecheGauche = new Rectangle(20, 300, 100, 100);
                 Rectangle flecheDroite = new Rectangle(960, 300, 100, 100);
 
+                if (afficheOutilsAvecObj)
+                {
+                    Rectangle objetZoomHitbox = new Rectangle(285, 150, 95, 430);
+
+                    if (mouseState.LeftButton == ButtonState.Pressed &&
+                        previousMouseState.LeftButton == ButtonState.Released)
+                    {
+                        if (objetZoomHitbox.Contains(mouseState.Position))
+                        {
+                            tournevisPris = true;
+                            AjouterMessage("Tournevis recupere.");
+                        }
+                        else
+                        {
+                            afficheOutilsAvecObj = false;
+                        }
+                    }
+
+                    previousMouseState = mouseState;
+                    base.Update(gameTime);
+                    return;
+                }
+
                 if (mouseState.LeftButton == ButtonState.Pressed &&
                     previousMouseState.LeftButton == ButtonState.Released)
                 {
@@ -183,22 +242,126 @@ namespace P_EscapeNexus
                     {
                         currentMurIndex++;
 
-                        if (currentMurIndex >= murTextures.Length)
-                            currentMurIndex = 0;
+                        if (!dans2emePiece)
+                        {
+                            if (currentMurIndex > 3) currentMurIndex = 0;
+                        }
+                        else
+                        {
+                            if (currentMurIndex > 5) currentMurIndex = 4;
+                        }
                     }
 
                     if (flecheDroite.Contains(mouseState.Position))
                     {
                         currentMurIndex--;
 
-                        if (currentMurIndex < 0)
-                            currentMurIndex = murTextures.Length - 1;
+                        if (!dans2emePiece)
+                        {
+                            if (currentMurIndex < 0) currentMurIndex = 3;
+                        }
+                        else
+                        {
+                            if (currentMurIndex < 4) currentMurIndex = 5;
+                        }
                     }
                 }
                 Rectangle caseHighlight1 = new Rectangle(30, 626, 110, 75);
                 Rectangle caseHighlight2 = new Rectangle(154, 626, 110, 75);
                 Rectangle caseHighlight3 = new Rectangle(278, 626, 110, 75);
                 Rectangle caseHighlight4 = new Rectangle(402, 626, 110, 75);
+
+                Rectangle porteHitbox = new Rectangle(365, 130, 285, 480);
+                Rectangle scanHitbox = new Rectangle(666, 328, 50, 80);
+                Rectangle casierHitbox = new Rectangle(777, 186, 150, 215);
+                Rectangle outilsHitbox = new Rectangle(573, 182, 230, 182);
+
+                bool badgeSelectionne = selectedInventoryCase == 1;
+                bool tournevisSelectionne = selectedInventoryCase == 2;
+
+                if (mouseState.LeftButton == ButtonState.Pressed &&
+                    previousMouseState.LeftButton == ButtonState.Released)
+                {
+                    // Scanner avec badge
+                    if (currentMurIndex == 0 &&
+                        scanHitbox.Contains(mouseState.Position) &&
+                        badgeSelectionne)
+                    {
+                        porteOuverte = true;
+                        AjouterMessage("Badge accepte. Porte ouverte.");
+                    }
+
+                    // Scanner sans badge
+                    else if (currentMurIndex == 0 &&
+                        scanHitbox.Contains(mouseState.Position) &&
+                        !badgeSelectionne)
+                    {
+                        AjouterMessage("Un badge est requis.");
+                    }
+
+                    // Inspecter les outils
+                    else if (currentMurIndex == 5 &&
+                        outilsHitbox.Contains(mouseState.Position))
+                    {
+                        afficheOutilsAvecObj = true;
+                        AjouterMessage("Vous inspectez les outils.");
+                    }
+
+                    // Ouvrir le panneau avec le tournevis
+                    else if (currentMurIndex == 2 &&
+                        casierHitbox.Contains(mouseState.Position) &&
+                        tournevisSelectionne &&
+                        tournevisPris)
+                    {
+                        panneauOuvert = true;
+                        AjouterMessage("Panneau ouvert avec le tournevis.");
+                    }
+
+                    // Cliquer sur le panneau sans tournevis
+                    else if (currentMurIndex == 2 &&
+                        casierHitbox.Contains(mouseState.Position) &&
+                        !tournevisSelectionne)
+                    {
+                        AjouterMessage("Il faut utiliser un outil.");
+                    }
+
+                    // Cliquer sur le panneau avec tournevis selectionne mais pas encore pris
+                    else if (currentMurIndex == 2 &&
+                        casierHitbox.Contains(mouseState.Position) &&
+                        tournevisSelectionne &&
+                        !tournevisPris)
+                    {
+                        AjouterMessage("Vous n'avez pas encore le tournevis.");
+                    }
+
+                    // Entrer dans la deuxieme piece
+                    else if (currentMurIndex == 0 &&
+                        porteHitbox.Contains(mouseState.Position) &&
+                        porteOuverte)
+                    {
+                        dans2emePiece = true;
+                        currentMurIndex = 4;
+                        AjouterMessage("Vous entrez dans la deuxieme piece.");
+                    }
+
+                    // Revenir dans la premiere piece
+                    else if (currentMurIndex == 4 &&
+                        porteHitbox.Contains(mouseState.Position) &&
+                        porteOuverte)
+                    {
+                        dans2emePiece = false;
+                        currentMurIndex = 0;
+                        AjouterMessage("Vous revenez dans la premiere piece.");
+                    }
+
+                    // Cliquer sur la porte fermee
+                    else if (currentMurIndex == 0 &&
+                        porteHitbox.Contains(mouseState.Position) &&
+                        !porteOuverte)
+                    {
+                        AjouterMessage("La porte est verrouillee.");
+                    }
+                }
 
                 if (mouseState.LeftButton == ButtonState.Pressed &&
                     previousMouseState.LeftButton == ButtonState.Released)
@@ -292,21 +455,84 @@ namespace P_EscapeNexus
             {
                 Rectangle flecheGauche = new Rectangle(10, 300, 100, 100);
                 Rectangle flecheDroite = new Rectangle(970, 300, 100, 100);
-                _spriteBatch.Draw(
-                    murTextures[currentMurIndex],
-                     new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
-                    Color.White
-                 ); 
+                Rectangle scanHitbox = new Rectangle(666, 328, 50, 80);
+                Rectangle porteHitbox = new Rectangle(365, 130, 285, 480);
+                Rectangle casierHitbox = new Rectangle(777, 186, 150, 215);
+                Rectangle outilsHitbox = new Rectangle(573, 182, 230, 182);
+
+
+                Texture2D murActuel = murTextures[currentMurIndex];
+
+                if (currentMurIndex == 0 && porteOuverte)
+                    murActuel = texturePorteOuverte;
+                if (currentMurIndex == 2 && panneauOuvert)
+                    murActuel = textureMurPanneauOuvert;
+
                 // Hitbox visibles
-                _spriteBatch.Draw(_debugTexture, flecheGauche, Color.Red * 0.4f);
-                _spriteBatch.Draw(_debugTexture, flecheDroite, Color.Red * 0.4f);
+                if (currentMurIndex == 0 || currentMurIndex == 4)
+                {
+                    // Scanner visible seulement quand la porte est fermée
+
+                    if (!porteOuverte)
+                        _spriteBatch.Draw(_debugTexture, scanHitbox, Color.Red * 0.4f);
+
+                    // Porte visible seulement quand elle est ouverte
+                    if (porteOuverte)
+                        _spriteBatch.Draw(_debugTexture, porteHitbox, Color.Red * 0.4f);
+                }
+                if (currentMurIndex == 2)
+                {
+                    _spriteBatch.Draw(_debugTexture, casierHitbox, Color.Red * 0.4f);
+                }
+                if (currentMurIndex == 5)
+                {
+                    _spriteBatch.Draw(_debugTexture, outilsHitbox, Color.Red * 0.4f);
+                }
+                if (currentMurIndex == 5 && tournevisPris)
+                {
+                    murActuel = textureMur6SansOutils;
+                }
+                _spriteBatch.Draw(
+                    murActuel,
+                    new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
+                Color.White
+                );
+
+                //_spriteBatch.Draw(_debugTexture, flecheGauche, Color.Red * 0.4f);
+                //_spriteBatch.Draw(_debugTexture, flecheDroite, Color.Red * 0.4f);
                 _spriteBatch.Draw(textureFlecheGauche, flecheGauche, Color.White * 0.4f);
                 _spriteBatch.Draw(textureFlecheDroite, flecheDroite, Color.White * 0.4f);
-                
+
                 //Rectangle commande = new Rectangle()
                 //_spriteBatch.Draw(_debugTexture, commande , )
 
+
+                if (afficheOutilsAvecObj)
+                {
+                    Texture2D imageZoom = textureOutilsAvecObj;
+
+                    if (tournevisPris)
+                        imageZoom = textureOutilsSansObj;
+
+                    _spriteBatch.Draw(
+                        imageZoom,
+                        new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
+                        Color.White
+                    );
+
+                    if (!tournevisPris)
+                    {
+                        Rectangle objetZoomHitbox = new Rectangle(285, 150, 95, 430);
+                        _spriteBatch.Draw(_debugTexture, objetZoomHitbox, Color.Green * 0.4f);
+                    }
+                }
+
                 _spriteBatch.Draw(textureInventaire, new Rectangle(0, 550, _graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 3), Color.White);
+                _spriteBatch.Draw(textureCommande, new Rectangle(720, 490, _graphics.PreferredBackBufferWidth / 3, _graphics.PreferredBackBufferHeight / 3), Color.White);
+
+                Vector2 position = new Vector2(745, 525);
+
+                
                 _spriteBatch.Draw(textureCommande, new Rectangle(720, 490, _graphics.PreferredBackBufferWidth / 3, _graphics.PreferredBackBufferHeight / 3), Color.White);
                 Rectangle caseHighlight1 = new Rectangle(30, 626, 110, 75);
                 Rectangle caseHighlight2 = new Rectangle(154, 626, 110, 75);
@@ -327,6 +553,19 @@ namespace P_EscapeNexus
                 if (selectedInventoryCase == 4)
                     DrawContour(caseHighlight4, Color.Yellow, 4);
                 _spriteBatch.Draw(textureBadge, new Rectangle(37, 630, _graphics.PreferredBackBufferWidth / 12, _graphics.PreferredBackBufferHeight / 11), Color.White);
+                if (tournevisPris)
+                {
+                    _spriteBatch.Draw(textureTournevis, new Rectangle(160, 630, 80, 65), Color.White);
+                }
+                for (int i = 0; i < messageConsole.Count; i++)
+                {
+                    _spriteBatch.DrawString(
+                        fontConsole,
+                        messageConsole[i],
+                        position + new Vector2(0, i * 25),
+                        Color.White
+                    );
+                }
             }
             _spriteBatch.End();
             base.Draw(gameTime);
